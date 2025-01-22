371c31ddc24c209381c53419e47c1f1c274cf9ac
Kyle W <125979751+MisterShika@users.noreply.github.com>
2025-01-22 23:33:05 +0900

    Figuring out React Router

---
+++ src/Home.jsx
@@ -0,0 +1,10 @@
+function Home () {
+    return(
+        <div className="home-page">
+            <img src="/src/assets/logo.jpg" className="logo" alt="Logo of this fake shopping website" />
+            Hello World.11
+        </div>
+    );
+};
+
+export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from './Home';
+import './style/Screen.css';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  // {
+  //   path: "profile",
+  //   element: <Profile />,
+  //   children: [
+  //     { index: true, element: <DefaultProfile /> },
+  //     { path: "spinach", element: <Spinach /> },
+  //     { path: "popeye", element: <Popeye /> },
+  //   ],
+  // },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
