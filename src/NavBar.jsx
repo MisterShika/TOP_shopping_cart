828fda4ec1111207acefe47c6d619f42c232dd92
Kyle W <125979751+MisterShika@users.noreply.github.com>
2025-01-27 20:35:01 +0900

    Very minor work because I'm getting the flu.

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,7 +1,8 @@
 function NavBar () {
     return(
         <nav className="nav-container">
-            
+            <input type="text"></input>
+            <button><i className="fa-solid fa-magnifying-glass"></i></button>
         </nav>
     )
 };
