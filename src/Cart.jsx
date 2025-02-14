4df5b4ea00cb452c87cb8c1a93381e12809a4dad
Kyle W <125979751+MisterShika@users.noreply.github.com>
2025-02-14 19:44:36 +0900

    Cart and product total costs now working.

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,10 +1,14 @@
 import { useEffect } from "react";
 
 function Cart ({cartData, setCartData}) {
+    const totalMath = cartData.reduce((total, item) => total + (item.price * item.quantity), 0);
+    const totalCost = totalMath.toFixed(2);
 
     useEffect(() => {
         
     }, [cartData]);
+
+    
 
     return (
         <div className="cart-container">
@@ -12,12 +16,14 @@ function Cart ({cartData, setCartData}) {
                 {
                     cartData.map((item) => {
                         return <li key={item.id}>
-                            <span className="item-name">{item.id}</span>
+                            <span className="item-name">{item.title}</span>
                             <span className="item-quantity">{item.quantity}</span>
+                            <span className="item-price-total">${item.price * item.quantity}</span>
                         </li>
                     })
                 }
             </ul>
+            <span className="cart-total">${totalCost}</span>
             <i className="fa-solid fa-cart-shopping"></i>
         </div>
     )
